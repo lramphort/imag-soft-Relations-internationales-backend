5be9f18527aa2db503eb3e499369eb340d7445f6
RAMPHORT Loic <loic.ramphort@soprasteria.com>
2020-02-20 06:55:03 +0100

    Revert "fic template.php"
    This reverts commit ad224f82ef7438f989be9ead487713350a298c50.

--- template.php
+++ template.php
@@ -3,8 +3,9 @@ header('Access-Control-Allow-Origin: *');
 $success = false;
 $data = array();
 include('db.php');
-function reponse_json($success="", $data="", $msgErreur=NULL) {
+function reponse_json($success, $data, $msgErreur=NULL) {
 	$array['success'] = $success;
 	$array['msg'] = $msgErreur;
 	$array['result'] = $data;
+	echo json_encode($data);
 }
